fe2cdd21467f4e833138f88c9a2693607329debf
OneMeanDragon <dragonkings@gmail.com>
2023-02-25 01:24:14 -0500

    Add files via upload

---
+++ SDL2NoLibs/SDL2Nolibs.cpp
@@ -0,0 +1,177 @@
+#include "SDL2Nolibs.h"
+
+CSDL* CSDL::sInstance = nullptr;
+bool CSDL::IsInitialized = false;
+
+CSDL* CSDL::Instance() {
+	if (sInstance == nullptr)
+		sInstance = new CSDL();
+
+	return sInstance;
+}
+
+void CSDL::Release()
+{
+	/* if anything else needs to be destroyed before this then do it above the instance deleteion */
+	delete sInstance;
+	sInstance = nullptr;
+}
+
+CSDL::CSDL() {
+	SDL_LOADED_DLL = LoadLibrary(SDL_MAIN_DLL);
+	if (SDL_LOADED_DLL)
+	{
+		SDL_Init = (sdl_init_t)GetProcAddress(SDL_LOADED_DLL, SDL_INIT_FUNCTION);
+		SDL_Quit = (sdl_quit_t)GetProcAddress(SDL_LOADED_DLL, SDL_QUIT_FUNCTION);
+		SDL_CreateWindow = (sdl_createwindow_t)GetProcAddress(SDL_LOADED_DLL, SDL_CREATEWINDOW_FUNCTION);
+		SDL_CreateRenderer = (sdl_createrenderer_t)GetProcAddress(SDL_LOADED_DLL, SDL_CREATERENDERER_FUNCTION);
+		SDL_SetRenderDrawColor = (sdl_setrenderdrawcolor_t)GetProcAddress(SDL_LOADED_DLL, SDL_SETRENDERDRAWCOLOR_FUNCTION);
+		SDL_CreateTextureFromSurface = (sdl_createtexturefromsurface_t)GetProcAddress(SDL_LOADED_DLL, SDL_CREATETEXTUREFROMSURFACE_FUNCTION);
+		SDL_FreeSurface = (sdl_freesurface_t)GetProcAddress(SDL_LOADED_DLL, SDL_FREESURFACE_FUNCTION);
+		SDL_PollEvent = (sdl_pollevent_t)GetProcAddress(SDL_LOADED_DLL, SDL_POLLEVENT_FUNCTION);
+		SDL_RenderClear = (sdl_renderclear_t)GetProcAddress(SDL_LOADED_DLL, SDL_RENDERCLEAR_FUNCTION);
+		SDL_RenderCopy = (sdl_rendercopy_t)GetProcAddress(SDL_LOADED_DLL, SDL_RENDERCOPY_FUNCTION);
+		SDL_RenderPresent = (sdl_renderpresent_t)GetProcAddress(SDL_LOADED_DLL, SDL_RENDERPRESENT_FUNCTION);
+
+		SDL_DestroyTexture = (sdl_destroytexture_t)GetProcAddress(SDL_LOADED_DLL, SDL_DESTROYTEXTURE_FUNCTION);
+		SDL_DestroyRenderer = (sdl_destroyrenderer_t)GetProcAddress(SDL_LOADED_DLL, SDL_DESTROYRENDERER_FUNCTION);
+		SDL_DestroyWindow = (sdl_destroywindow_t)GetProcAddress(SDL_LOADED_DLL, SDL_DESTROYWINDOW_FUNCTION);
+
+		SDL_GetError = (sdl_geterror_t)GetProcAddress(SDL_LOADED_DLL, SDL_GETERROR_FUNCTION);
+
+		SDL_RenderCopyEx = (sdl_rendercopyex_t)GetProcAddress(SDL_LOADED_DLL, SDL_RENDERCOPYEX_FUNCTION);
+		SDL_RenderDrawLine = (sdl_renderdrawline_t)GetProcAddress(SDL_LOADED_DLL, SDL_RENDERDRAWLINE_FUNCTION);
+
+		SDL_GetBasePath = (sdl_geterror_t)GetProcAddress(SDL_LOADED_DLL, SDL_GETBASEPATH_FUNCTION);
+
+		SDL_GetTicks = (sdl_getticks_t)GetProcAddress(SDL_LOADED_DLL, SDL_GETTICKS_FUNCTION);
+		SDL_QueryTexture = (sdl_querytexture_t)GetProcAddress(SDL_LOADED_DLL, SDL_QUERYTEXTURE_FUNCTION);
+
+		SDL_GetKeyboardState = (sdl_getkeyboardstate_t)GetProcAddress(SDL_LOADED_DLL, SDL_GETKEYBOARDSTATE_FUNCTION);
+		SDL_GetMouseState = (sdl_getmousestate_t)GetProcAddress(SDL_LOADED_DLL, SDL_GETMOUSESTATE_FUNCTION);
+		SDL_RWFromFile = (sdl_rwfromfile_t)GetProcAddress(SDL_LOADED_DLL, SDL_RWFROMFILE_FUNCTION);
+
+		IsInitialized = true;
+	}
+}
+
+CSDL::~CSDL() {
+	if (SDL_LOADED_DLL) {
+		FreeLibrary(SDL_LOADED_DLL);
+	}
+}
+
+/* SDL2_DLL */
+int32_t CSDL::Init(uint32_t dwFlag) {
+	return Instance()->SDL_Init(dwFlag);
+}
+
+void CSDL::Quit(void)
+{
+	Instance()->SDL_Quit();
+	return Instance()->Release();
+}
+
+const char* CSDL::GetError(void)
+{
+	return Instance()->SDL_GetError();
+}
+
+SDL_Window* CSDL::CreateHWindow(const char* title, int32_t x, int32_t y, int32_t w, int32_t h, uint32_t flags)
+{
+	return Instance()->SDL_CreateWindow(title, x, y, w, h, flags);
+}
+
+SDL_Renderer* CSDL::CreateRenderer(SDL_Window* window, int32_t index, uint32_t flags)
+{
+	return Instance()->SDL_CreateRenderer(window, index, flags);
+}
+
+int32_t CSDL::SetRenderDrawColor(SDL_Renderer* renderer, uint8_t r, uint8_t g, uint8_t b, uint8_t a)
+{
+	return Instance()->SDL_SetRenderDrawColor(renderer, r, g, b, a);
+}
+
+SDL_Texture* CSDL::CreateTextureFromSurface(SDL_Renderer* renderer, SDL_Surface* surface) {
+	return Instance()->SDL_CreateTextureFromSurface(renderer, surface);
+}
+
+void CSDL::FreeSurface(SDL_Surface* surface)
+{
+	return Instance()->SDL_FreeSurface(surface);
+}
+
+int32_t CSDL::PollEvent(SDL_Event* sevent)
+{
+	return Instance()->SDL_PollEvent(sevent);
+}
+
+int32_t CSDL::RenderClear(SDL_Renderer* renderer)
+{
+	return Instance()->SDL_RenderClear(renderer);
+}
+
+int32_t CSDL::RenderCopy(SDL_Renderer* renderer, SDL_Texture* texture, const SDL_Rect* srcrect, const SDL_Rect* dstrect)
+{
+	return Instance()->SDL_RenderCopy(renderer, texture, srcrect, dstrect);
+}
+
+void CSDL::RenderPresent(SDL_Renderer* renderer)
+{
+	return Instance()->SDL_RenderPresent(renderer);
+}
+
+void CSDL::DestroyTexture(SDL_Texture* texture)
+{
+	return Instance()->SDL_DestroyTexture(texture);
+}
+
+void CSDL::DestroyRenderer(SDL_Renderer* renderer)
+{
+	return Instance()->SDL_DestroyRenderer(renderer);
+}
+
+void CSDL::DestroyWindow(SDL_Window* window)
+{
+	return Instance()->SDL_DestroyWindow(window);
+}
+
+int32_t CSDL::RenderCopyEx(SDL_Renderer* renderer, SDL_Texture* texture, const SDL_Rect* srcrect, const SDL_Rect* dstrect, const double angle, const SDL_Point* center, const SDL_RendererFlip flip)
+{
+	return Instance()->SDL_RenderCopyEx(renderer, texture, srcrect, dstrect, angle, center, flip);
+}
+
+int32_t CSDL::RenderDrawLine(SDL_Renderer* renderer, int32_t x1, int32_t y1, int32_t x2, int32_t y2)
+{
+	return Instance()->SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
+}
+
+const char* CSDL::GetBasePath(void)
+{
+	return Instance()->SDL_GetBasePath();
+}
+
+uint32_t CSDL::GetTicks(void)
+{
+	return Instance()->SDL_GetTicks();
+}
+
+int32_t CSDL::QueryTexture(SDL_Texture* texture, uint32_t* format, int32_t* access, int32_t* w, int32_t* h)
+{
+	return Instance()->SDL_QueryTexture(texture, format, access, w, h);
+}
+
+const uint8_t* CSDL::GetKeyboardState(int32_t* numkeys)
+{
+	return Instance()->SDL_GetKeyboardState(numkeys);
+}
+
+uint32_t CSDL::GetMouseState(int32_t* x, int32_t* y)
+{
+	return Instance()->SDL_GetMouseState(x, y);
+}
+
+SDL_RWops* CSDL::RWFromFile(const char* file, const char* mode)
+{
+	return Instance()->SDL_RWFromFile(file, mode);
+}
---
+++ SDL2NoLibs/SDL2Nolibs.h
@@ -0,0 +1,146 @@
+#if !defined(SDL_INITIALIZER_H)
+#define SDL_INITIALIZER_H
+#pragma once
+
+#define VC_EXTRALEAN
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+
+#include <libloaderapi.h>
+#include <cinttypes>
+
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+
+#define SDL_MAIN_DLL TEXT("SDL2.DLL")
+#define SDL_INIT_FUNCTION "SDL_Init"
+#define SDL_CREATEWINDOW_FUNCTION "SDL_CreateWindow"
+#define SDL_CREATERENDERER_FUNCTION "SDL_CreateRenderer"
+#define SDL_SETRENDERDRAWCOLOR_FUNCTION "SDL_SetRenderDrawColor"
+#define SDL_CREATETEXTUREFROMSURFACE_FUNCTION "SDL_CreateTextureFromSurface"
+#define SDL_FREESURFACE_FUNCTION "SDL_FreeSurface"
+#define SDL_POLLEVENT_FUNCTION "SDL_PollEvent"
+#define SDL_RENDERCLEAR_FUNCTION "SDL_RenderClear"
+#define SDL_RENDERCOPY_FUNCTION "SDL_RenderCopy"
+#define SDL_RENDERPRESENT_FUNCTION "SDL_RenderPresent"
+/* destroys */
+#define SDL_DESTROYTEXTURE_FUNCTION "SDL_DestroyTexture"
+#define SDL_DESTROYRENDERER_FUNCTION "SDL_DestroyRenderer"
+#define SDL_DESTROYWINDOW_FUNCTION "SDL_DestroyWindow"
+/* quit */
+#define SDL_QUIT_FUNCTION "SDL_Quit"
+/* error */
+#define SDL_GETERROR_FUNCTION "SDL_GetError"
+#define SDL_RENDERCOPYEX_FUNCTION "SDL_RenderCopyEx"
+#define SDL_RENDERDRAWLINE_FUNCTION "SDL_RenderDrawLine"
+#define SDL_GETBASEPATH_FUNCTION "SDL_GetBasePath"
+#define SDL_GETTICKS_FUNCTION "SDL_GetTicks"
+#define SDL_QUERYTEXTURE_FUNCTION "SDL_QueryTexture"
+#define SDL_GETKEYBOARDSTATE_FUNCTION "SDL_GetKeyboardState"
+#define SDL_GETMOUSESTATE_FUNCTION "SDL_GetMouseState"
+#define SDL_RWFROMFILE_FUNCTION "SDL_RWFromFile"
+
+/*
+extern DECLSPEC SDL_RWops *SDLCALL SDL_RWFromFile(const char *file, const char *mode);
+*/
+
+typedef int32_t(*sdl_init_t)(uint32_t flags);
+typedef void(*sdl_quit_t)(void);
+typedef const char* (*sdl_geterror_t)(void);
+
+typedef SDL_Window* (*sdl_createwindow_t)(const char* title, int32_t x, int32_t y, int32_t w, int32_t h, uint32_t flags);
+typedef SDL_Renderer* (*sdl_createrenderer_t)(SDL_Window* window, int32_t index, uint32_t flags);
+typedef int32_t(*sdl_setrenderdrawcolor_t)(SDL_Renderer* renderer, uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+typedef int32_t(*sdl_pollevent_t)(SDL_Event* sevent);
+typedef int32_t(*sdl_renderclear_t)(SDL_Renderer* renderer);
+typedef int32_t(*sdl_rendercopy_t)(SDL_Renderer* renderer, SDL_Texture* texture, const SDL_Rect* srcrect, const SDL_Rect* dstrect);
+typedef void (*sdl_renderpresent_t)(SDL_Renderer* renderer);
+typedef void (*sdl_destroytexture_t)(SDL_Texture* texture);
+typedef void (*sdl_destroyrenderer_t)(SDL_Renderer* renderer);
+typedef void (*sdl_destroywindow_t)(SDL_Window* window);
+typedef SDL_Texture* (*sdl_createtexturefromsurface_t)(SDL_Renderer* renderer, SDL_Surface* surface);
+typedef void (*sdl_freesurface_t)(SDL_Surface* surface);
+typedef int32_t(*sdl_rendercopyex_t)(SDL_Renderer* renderer, SDL_Texture* texture, const SDL_Rect* srcrect, const SDL_Rect* dstrect, const double angle, const SDL_Point* center, const SDL_RendererFlip flip);
+typedef int32_t(*sdl_renderdrawline_t)(SDL_Renderer* renderer, int32_t x1, int32_t y1, int32_t x2, int32_t y2);
+typedef uint32_t(*sdl_getticks_t)(void);
+typedef int32_t(*sdl_querytexture_t)(SDL_Texture* texture, uint32_t* format, int32_t* access, int32_t* w, int32_t* h);
+typedef const uint8_t*(*sdl_getkeyboardstate_t)(int32_t* numkeys);
+typedef uint32_t(*sdl_getmousestate_t)(int32_t* x, int32_t* y);
+typedef SDL_RWops*(*sdl_rwfromfile_t)(const char* file, const char* mode);
+
+class CSDL {
+private:
+	static CSDL* sInstance;
+
+	CSDL();
+	~CSDL();
+
+public:
+	static bool IsInitialized;
+
+	static CSDL* Instance();
+	static void Release();
+
+	static int32_t Init(uint32_t dwFlag);
+	static void Quit(void);
+	static const char* GetError(void);
+
+	static SDL_Window* CreateHWindow(const char* title, int32_t x, int32_t y, int32_t w, int32_t h, uint32_t flags);
+	static SDL_Renderer* CreateRenderer(SDL_Window* window, int32_t index, uint32_t flags);
+	static int32_t SetRenderDrawColor(SDL_Renderer* renderer, uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+	static SDL_Texture* CreateTextureFromSurface(SDL_Renderer* renderer, SDL_Surface* surface);
+	static void FreeSurface(SDL_Surface* surface);
+	static int32_t PollEvent(SDL_Event* sevent);
+	static int32_t RenderClear(SDL_Renderer* renderer);
+	static int32_t RenderCopy(SDL_Renderer* renderer, SDL_Texture* texture, const SDL_Rect* srcrect, const SDL_Rect* dstrect);
+	static void RenderPresent(SDL_Renderer* renderer);
+	static void DestroyTexture(SDL_Texture* texture);
+	static void DestroyRenderer(SDL_Renderer* renderer);
+	static void DestroyWindow(SDL_Window* window);
+
+	static int32_t RenderCopyEx(SDL_Renderer* renderer, SDL_Texture* texture, const SDL_Rect* srcrect, const SDL_Rect* dstrect, const double angle, const SDL_Point* center, const SDL_RendererFlip flip);
+	static int32_t RenderDrawLine(SDL_Renderer* renderer, int32_t x1, int32_t y1, int32_t x2, int32_t y2);
+	static const char* GetBasePath(void);
+	static uint32_t GetTicks(void);
+	static int32_t QueryTexture(SDL_Texture* texture, uint32_t* format, int32_t* access, int32_t* w, int32_t* h);
+
+	static const uint8_t* GetKeyboardState(int32_t* numkeys);
+	static uint32_t GetMouseState(int32_t* x, int32_t* y);
+	static SDL_RWops* RWFromFile(const char* file, const char* mode);
+
+private:
+	/* SDL2 Initialization calls */
+	sdl_init_t SDL_Init = nullptr;
+
+	/* sdl2 */
+	sdl_createwindow_t SDL_CreateWindow = nullptr;
+	sdl_createrenderer_t SDL_CreateRenderer = nullptr;
+	sdl_setrenderdrawcolor_t SDL_SetRenderDrawColor = nullptr;
+	sdl_createtexturefromsurface_t SDL_CreateTextureFromSurface = nullptr;
+	sdl_freesurface_t SDL_FreeSurface = nullptr;
+	sdl_pollevent_t SDL_PollEvent = nullptr;
+	sdl_renderclear_t SDL_RenderClear = nullptr;
+	sdl_rendercopy_t SDL_RenderCopy = nullptr;
+	sdl_renderpresent_t SDL_RenderPresent = nullptr;
+	sdl_destroytexture_t SDL_DestroyTexture = nullptr;
+	sdl_destroyrenderer_t SDL_DestroyRenderer = nullptr;
+	sdl_destroywindow_t SDL_DestroyWindow = nullptr;
+	sdl_quit_t SDL_Quit = nullptr;
+	sdl_geterror_t SDL_GetError = nullptr;
+	sdl_rendercopyex_t SDL_RenderCopyEx = nullptr;
+	sdl_renderdrawline_t SDL_RenderDrawLine = nullptr;
+	sdl_geterror_t SDL_GetBasePath = nullptr;
+	sdl_getticks_t SDL_GetTicks = nullptr;
+	sdl_querytexture_t SDL_QueryTexture = nullptr;
+	sdl_getkeyboardstate_t SDL_GetKeyboardState = nullptr;
+	sdl_getmousestate_t SDL_GetMouseState = nullptr;
+	sdl_rwfromfile_t SDL_RWFromFile = nullptr;
+
+private:
+	HINSTANCE SDL_LOADED_DLL;
+};
+
+
+
+#endif
