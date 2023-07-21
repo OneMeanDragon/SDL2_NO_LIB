d0a3472b8680e1c1fc0b4b2432509a591ee72159
OneMeanDragon <dragonkings@gmail.com>
2023-07-21 19:32:21 -0400

    Update SDL2Nolibs.h
    Additional renderer options.

--- SDL2NoLibs/SDL2Nolibs.h
+++ SDL2NoLibs/SDL2Nolibs.h
@@ -16,7 +16,6 @@
 #define SDL_MAIN_DLL TEXT("SDL2.DLL")
 #define SDL_INIT_FUNCTION "SDL_Init"
 #define SDL_CREATEWINDOW_FUNCTION "SDL_CreateWindow"
-#define SDL_CREATERENDERER_FUNCTION "SDL_CreateRenderer"
 #define SDL_SETRENDERDRAWCOLOR_FUNCTION "SDL_SetRenderDrawColor"
 #define SDL_CREATETEXTUREFROMSURFACE_FUNCTION "SDL_CreateTextureFromSurface"
 #define SDL_FREESURFACE_FUNCTION "SDL_FreeSurface"
@@ -40,9 +39,14 @@
 #define SDL_GETKEYBOARDSTATE_FUNCTION "SDL_GetKeyboardState"
 #define SDL_GETMOUSESTATE_FUNCTION "SDL_GetMouseState"
 #define SDL_RENDERFILLRECT_FUNCTION "SDL_RenderFillRect"
-#define SDL_GETPERFORMANCECOUNTER "SDL_GetPerformanceCounter"
-#define SDL_GETPERFORMANCEFREQUENCY "SDL_GetPerformanceFrequency"
-#define SDL_DELAY "SDL_Delay"
+#define SDL_GETPERFORMANCECOUNTER_FUNCTION "SDL_GetPerformanceCounter"
+#define SDL_GETPERFORMANCEFREQUENCY_FUNCTION "SDL_GetPerformanceFrequency"
+#define SDL_DELAY_FUNCTION "SDL_Delay"
+
+/* Renderer Inits */
+#define SDL_CREATERENDERER_FUNCTION "SDL_CreateRenderer"
+#define SDL_GETNUMRENDERDRIVERS_FUNCTION "SDL_GetNumRenderDrivers"
+#define SDL_GETNUMRENDERDRIVERINFO_FUNCTION "SDL_GetRenderDriverInfo"
 
 /* Memory Func */
 #define SDL_RWFROMFILE_FUNCTION "SDL_RWFromFile"
@@ -50,16 +54,11 @@
 #define SDL_RWFROMCONSTMEM_FUNCTION "SDL_RWFromConstMem"
 
 
-/*
-extern DECLSPEC int SDLCALL SDL_RenderFillRect(SDL_Renderer * renderer, const SDL_Rect * rect);
-*/
-
 typedef int32_t(*sdl_init_t)(uint32_t flags);
 typedef void(*sdl_quit_t)(void);
 typedef const char* (*sdl_geterror_t)(void);
 
 typedef SDL_Window* (*sdl_createwindow_t)(const char* title, int32_t x, int32_t y, int32_t w, int32_t h, uint32_t flags);
-typedef SDL_Renderer* (*sdl_createrenderer_t)(SDL_Window* window, int32_t index, uint32_t flags);
 typedef int32_t(*sdl_setrenderdrawcolor_t)(SDL_Renderer* renderer, uint8_t r, uint8_t g, uint8_t b, uint8_t a);
 typedef int32_t(*sdl_pollevent_t)(SDL_Event* sevent);
 typedef int32_t(*sdl_renderclear_t)(SDL_Renderer* renderer);
@@ -77,9 +76,14 @@ typedef int32_t(*sdl_querytexture_t)(SDL_Texture* texture, uint32_t* format, int
 typedef const uint8_t* (*sdl_getkeyboardstate_t)(int32_t* numkeys);
 typedef uint32_t(*sdl_getmousestate_t)(int32_t* x, int32_t* y);
 typedef int32_t(*sdl_renderfillrect_t)(SDL_Renderer* renderer, const SDL_Rect* rect);
-typedef uint64_t(*sdl_getperformancecounter)();
-typedef uint64_t(*sdl_getperformancefrequency)();
-typedef void(*sdl_delay)(uint32_t);
+typedef uint64_t(*sdl_getperformancecounter_t)();
+typedef uint64_t(*sdl_getperformancefrequency_t)();
+typedef void(*sdl_delay_t)(uint32_t);
+// Renderer Inits
+typedef SDL_Renderer* (*sdl_createrenderer_t)(SDL_Window* window, int32_t index, uint32_t flags);
+typedef int32_t(*sdl_getnumrenderdrivers_t)(void);
+typedef int32_t(*sdl_getnumrenderdriverinfo_t)(int32_t index, SDL_RendererInfo* info);
+
 // Memory functions
 typedef SDL_RWops* (*sdl_rwfromfile_t)(const char* file, const char* mode);
 typedef SDL_RWops* (*sdl_rwfrommem_t)(void* mem, int size);
@@ -104,7 +108,6 @@ public:
 	static const char* GetError(void);
 
 	static SDL_Window* CreateHWindow(const char* title, int32_t x, int32_t y, int32_t w, int32_t h, uint32_t flags);
-	static SDL_Renderer* CreateRenderer(SDL_Window* window, int32_t index, uint32_t flags);
 	static int32_t SetRenderDrawColor(SDL_Renderer* renderer, uint8_t r, uint8_t g, uint8_t b, uint8_t a);
 	static SDL_Texture* CreateTextureFromSurface(SDL_Renderer* renderer, SDL_Surface* surface);
 	static void FreeSurface(SDL_Surface* surface);
@@ -132,6 +135,13 @@ public:
 	static uint64_t GetPerformanceFrequency();
 	static void Delay(uint32_t ms);
 
+	// Renderer Inits
+	static SDL_Renderer* CreateRenderer(SDL_Window* window, int32_t index, uint32_t flags);
+	static int32_t GetNumRenderDrivers(void);
+	static int32_t GetRenderDriverInfo(int32_t index, SDL_RendererInfo* info);
+
+
+
 	// Memory func
 	static SDL_RWops* RWFromFile(const char* file, const char* mode);
 	static SDL_RWops* RWFromMem(void* mem, int size);
@@ -143,7 +153,6 @@ private:
 
 	/* sdl2 */
 	sdl_createwindow_t SDL_CreateWindow = nullptr;
-	sdl_createrenderer_t SDL_CreateRenderer = nullptr;
 	sdl_setrenderdrawcolor_t SDL_SetRenderDrawColor = nullptr;
 	sdl_createtexturefromsurface_t SDL_CreateTextureFromSurface = nullptr;
 	sdl_freesurface_t SDL_FreeSurface = nullptr;
@@ -164,83 +173,21 @@ private:
 	sdl_getkeyboardstate_t SDL_GetKeyboardState = nullptr;
 	sdl_getmousestate_t SDL_GetMouseState = nullptr;
 	sdl_renderfillrect_t SDL_RenderFillRect = nullptr;
-	sdl_getperformancecounter SDL_GetPerformanceCounter = nullptr;
-	sdl_getperformancefrequency SDL_GetPerformanceFrequency = nullptr;
-	sdl_delay SDL_Delay = nullptr;
+	sdl_getperformancecounter_t SDL_GetPerformanceCounter = nullptr;
+	sdl_getperformancefrequency_t SDL_GetPerformanceFrequency = nullptr;
+	sdl_delay_t SDL_Delay = nullptr;
+	//renderer inits
+	sdl_createrenderer_t SDL_CreateRenderer = nullptr;
+	sdl_getnumrenderdrivers_t SDL_GetNumRenderDrivers = nullptr;
+	sdl_getnumrenderdriverinfo_t SDL_GetRenderDriverInfo = nullptr;
 	//memory func
 	sdl_rwfromfile_t SDL_RWFromFile = nullptr;
 	sdl_rwfrommem_t SDL_RWFromMem = nullptr;
 	sdl_rwfromconstmem_t SDL_RWFromConstMem = nullptr;
 
+
 private:
 	HINSTANCE SDL_LOADED_DLL;
 };
-
-
-//#pragma region "TIMER"
-//class Timer {
-//
-//private:
-//	//Needed to make Timer a singleton class
-//	static Timer* sInstance;
-//
-//	//Contains the time of the last reset
-//	unsigned int mStartTicks;
-//	//Stores the number of milliseconds since the last reset
-//	unsigned int mElapsedTicks;
-//	//Stores the time elapsed since the last reset in seconds
-//	float mDelataTime;
-//	//Can be used to speed up or slowdown all entities that transform using it
-//	float mTimeScale;
-//
-//public:
-//	//-----------------------------------------
-//	//Returns a pointer to the class instance  
-//	//-----------------------------------------
-//	static Timer* Instance();
-//	//-----------------------------------------------------
-//	//Releases the class instance and sets it back to NULL 
-//	//-----------------------------------------------------
-//	static void Release();
-//
-//	//------------------------------------------------------------------------
-//	//Resets the time elapsed back to 0, is usually called after each frame
-//	//so that mDeltaTime is the time elapsed since last frame
-//	//------------------------------------------------------------------------
-//	void Reset();
-//
-//	//-------------------------------------------------------
-//	//Time elapsed in seconds since the last Reset
-//	//-------------------------------------------------------
-//	float DeltaTime();
-//
-//	//---------------------------------------------------------------
-//	//Sets the TimeScale default value: 1.0f
-//	//---------------------------------------------------------------
-//	void TimeScale(float t = 1.0f);
-//	//---------------------------------------------------------------
-//	//Can be used to speed up or slowdown all transformations
-//	//by multiplying it by the change in position or rotation
-//	//---------------------------------------------------------------
-//	float TimeScale();
-//
-//	//--------------------------------------------------------------------
-//	//Should be called in the EarlyUpdate, updates the time elapsed,
-//	//as well as the DeltaTime since the last Reset
-//	//--------------------------------------------------------------------
-//	void Update();
-//
-//private:
-//
-//	//------------------------------------------------------------------------------------------
-//	//Contructor is private so that Instance() must be used to get an instance when needed  
-//	//------------------------------------------------------------------------------------------
-//	Timer();
-//	//--------------------------------------------------------------------------------------
-//	//Destructor is private so that the instance can only be destroyed using Release()  
-//	//--------------------------------------------------------------------------------------
-//	~Timer();
-//};
-//#pragma endregion
 
 #endif
